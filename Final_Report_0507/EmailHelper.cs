62e624a56c64584a9d560ac40626a6f8d258d76b
黃晉瑋 <little0ghost@gmail.com>
2025-05-14 21:31:45 +0800

    +Import/Export system

--- Final_Report_0507/EmailHelper.cs
+++ Final_Report_0507/EmailHelper.cs
@@ -12,14 +12,14 @@ namespace Final_Report_0507
         public static async Task SendMailAsync(string toEmail, string subject, string body)
         {
             var message = new MailMessage();
-            message.From = new MailAddress("your_email@gmail.com"); // 換成你的寄件者
+            message.From = new MailAddress("litnotify.service@gmail.com"); // 換成你的寄件者
             message.To.Add(toEmail);
             message.Subject = subject;
             message.Body = body;
 
             using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
             {
-                smtpClient.Credentials = new NetworkCredential("your_email@gmail.com", "your_app_password");
+                smtpClient.Credentials = new NetworkCredential("litnotify.service@gmail.com", "pbqg avfg hgog okxd");
                 smtpClient.EnableSsl = true;
 
                 await smtpClient.SendMailAsync(message);  // 🔹 非同步寄信
