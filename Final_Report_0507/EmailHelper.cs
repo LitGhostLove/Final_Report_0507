6dadfdff08e051b7c5da41259544d7d9b690295b
黃晉瑋 <little0ghost@gmail.com>
2025-05-14 21:46:26 +0800

    update

---
+++ Final_Report_0507/EmailHelper.cs
@@ -0,0 +1,29 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Net.Mail;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Final_Report_0507
+{
+    public static class EmailHelper
+    {
+        public static async Task SendMailAsync(string toEmail, string subject, string body)
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress("litnotify.service@gmail.com"); // 換成你的寄件者
+            message.To.Add(toEmail);
+            message.Subject = subject;
+            message.Body = body;
+
+            using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+            {
+                smtpClient.Credentials = new NetworkCredential("litnotify.service@gmail.com", "pbqg avfg hgog okxd");
+                smtpClient.EnableSsl = true;
+
+                await smtpClient.SendMailAsync(message);  // 🔹 非同步寄信
+            }
+        }
+    }
+}
